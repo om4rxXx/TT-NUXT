27f617d8c1ce9be3a39d719a94776f9cad90db37
om4rxXx <wilson030082017@gmail.com>
2023-02-22 15:46:07 -0600

    ai quedo

--- pages/Main/Users.vue
+++ pages/Main/Users.vue
@@ -31,7 +31,112 @@
             class="elevation-1"
             :search="search"
             elevation="14"
+            @click="XD"
           >
+            <template v-slot:[`item.status`]="{ item }">
+              <v-dialog v-model="dialog" width="500">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    v-bind="attrs"
+                    v-on="on"
+                    small
+                    rounded
+                    :color="getColor(item.status)"
+                    dark
+                    >{{ item.status }}</v-btn
+                  >
+                </template>
+                <v-card class="rounded-xl">
+                  <div class="d-flex justify-center pt-7">
+                    <v-icon large color="yellow" dark> mdi-alert </v-icon>
+                  </div>
+                  <div class="d-flex justify-center pt-7">
+                    <h3>Aceptación de usuario</h3>
+                  </div>
+                  <div class="d-flex justify-center pt-7">
+                    ¿Está seguro que desea aceptar al usuario?
+                  </div>
+                  <v-card-actions class="d-flex justify-center">
+                    <div class="d-flex justify-center">
+                      <v-btn
+                        height="50"
+                        class="btn-center white--text rounded-xl ma-6"
+                        depressed
+                        color="#34A853"
+                        :loading="loading"
+                        @click="editarUser"
+                      >
+                        ACEPTAR
+                      </v-btn>
+                      <v-btn
+                        height="50"
+                        class="btn-center white--text rounded-xl ma-6"
+                        depressed
+                        color="#FF5252"
+                        :loading="loading"
+                      >
+                        Cancelar
+                      </v-btn>
+                    </div>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </template>
+            <template v-slot:[`item.mascotas`]="{ item }">
+              <v-chip class="ma-2" color="#5CBBF6" dark>
+                {{ item.mascotas }}
+              </v-chip>
+            </template>
+            <template v-slot:[`item.baja`]>
+              <v-dialog v-model="dialog" width="500">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    v-bind="attrs"
+                    v-on="on"
+                    class="ma-2"
+                    text
+                    icon
+                    color="red lighten-2"
+                  >
+                    <v-icon>mdi-delete</v-icon>
+                  </v-btn>
+                </template>
+                <v-card class="rounded-xl">
+                  <div class="d-flex justify-center pt-7">
+                    <v-icon large color="yellow" dark> mdi-alert </v-icon>
+                  </div>
+                  <div class="d-flex justify-center pt-7">
+                    <h3>Dar de baja usuario</h3>
+                  </div>
+                  <div class="d-flex justify-center pt-7">
+                    ¿Está seguro que desea dar de baja al usuario?
+                  </div>
+                  <v-card-actions class="d-flex justify-center">
+                    <div class="d-flex justify-center">
+                      <v-btn
+                        height="50"
+                        class="btn-center white--text rounded-xl ma-6"
+                        depressed
+                        color="#34A853"
+                        :loading="loading"
+                        @click="editarUser"
+                      >
+                        ACEPTAR
+                      </v-btn>
+                      <v-btn
+                        height="50"
+                        class="btn-center white--text rounded-xl ma-6"
+                        depressed
+                        color="#FF5252"
+                        :loading="loading"
+                      >
+                        Cancelar
+                      </v-btn>
+                    </div>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </template>
           </v-data-table>
         </v-card>
       </div>
@@ -53,100 +158,58 @@ export default {
     return {
       search: "",
 
+      headers: [
+        {
+          text: "Nombre",
+          align: "center",
+
+          value: "name",
+        },
+        { text: "Estado de Solicitud", align: "center", value: "status" },
+        { text: "Mascotas", value: "mascotas", align: "center" },
+        { text: "Dar de baja", value: "baja", align: "center" },
+      ],
       desserts: [
         {
           name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: 1,
+          status: "Aceptado",
+          mascotas: ("tsao", "sd"),
+          baja: 24,
         },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: 1,
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: 7,
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: 8,
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: 16,
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: 0,
-        },
+
         {
           name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: 2,
+          status: "Pendiente",
+          mascotas: "juan",
+          baja: 54,
+        },
+        {
+          name: "Honeycomb",
+          status: "Rechazado",
+          mascotas: "Peso pluma",
+          baja: 55,
+        },
+        {
+          name: "Donut",
+          status: "Aceptado",
+          mascotas: "junior h",
+          baja: 55,
+        },
+        {
+          name: "KitKat",
+          status: "Aceptado",
+          mascotas: "junior h",
+          baja: 55,
         },
       ],
     };
   },
-  computed: {
-    headers() {
-      return [
-        {
-          text: "Nombre",
-          align: "start",
-          sortable: false,
-          value: "name",
-        },
-
-        {
-          text: "Estado de Solicitud",
-          align: "Center",
-          value: "verified",
-          sortable: false,
-          value: "name",
-        },
-        {
-          text: "Mascotas",
-          align: "Center",
-          sortable: false,
-          value: "name",
-        },
-
-        {
-          text: "Dar de baja",
-          align: "Center",
-          sortable: false,
-          value: "name",
-        },
-      ];
-    },
-  },
   methods: {
+    getColor(calories) {
+      if (calories == "Rechazado") return "red";
+      else if (calories == "Pendiente") return "orange";
+      else return "green";
+    },
     filterOnlyCapsText(value, search, item) {
       return (
         value != null &&
@@ -154,6 +217,9 @@ export default {
         typeof value === "string" &&
         value.toString().toLocaleUpperCase().indexOf(search) !== -1
       );
+    },
+    XD() {
+      this.$router.push("https://v2.vuetifyjs.com/en/api/v-data-table/#props");
     },
   },
 };
